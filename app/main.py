72d94d56a387278b04cdda0b87ec64f5d9f5bb9c
Mawande14 <220246009@mycput.ac.za>
2026-09-22 03:48:08 +0200

    Allow Netlify frontend origin

--- app/main.py
+++ app/main.py
@@ -16,7 +16,7 @@ def startup_event():
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173", "https://simplycomplexafrica.netlify.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
